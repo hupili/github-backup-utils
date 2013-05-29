a3ee291a28a3d5c820a4923521798137a9219a00
hupili <hpl1989@gmail.com>
2013-05-29 19:38:23 +0800

    an auto script for cron

---
+++ auto.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+echo "[start]" `date` >> log
+./update-repo-list.sh 2>&1 | tee -a log &> /dev/null
+./backup.sh 2>&1 | tee -a log &> /dev/null
+echo "[end]" `date` >> log
+
+exit 0
